8ee8ceae4831851c4cc80a2ae2e0e0ff33f2ba4c
xudazhou <shin.12@163.com>
2018-05-25 10:22:42 +0800

    fordemo

--- whiledemo.cpp
+++ whiledemo.cpp
@@ -3,10 +3,15 @@ using namespace std;
 
 int main()
 {
-	int i1 = 0;
-	while (i1 < 3) {
+	// int i1 = 0;
+	// while (i1 < 3) {
+	// 	cout << i1 << endl;
+	// 	i1++;
+	// }
+
+	for (int i1 = 1; i1 < 3; i1++) {
 		cout << i1 << endl;
-		i1++;
 	}
+
 	return 0;
 }
